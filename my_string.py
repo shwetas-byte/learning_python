925a665134c71c9cf987733fe432704c08138d4a
Shweta Singh <shwetas6913@gmail.com>
2026-03-31 13:34:41 +0530

    Learning Python

--- my_string.py
+++ my_string.py
@@ -1,7 +1,40 @@
-s=input("Enter your name:")
-print(s)
-print(f'Type of {s} is {type(s)}')
-print(f'ID of {s} is {id(s)}')
-print(f'Max of {s} is {max(s)}')
-print(f'Min of {s} is {min(s)}')
-print(f'Length of {s} is {len(s)}')
+# s=input("Enter your name:")
+# print(s)
+# print(f'Type of {s} is {type(s)}')
+# print(f'ID of {s} is {id(s)}')
+# print(f'Max of {s} is {max(s)}')
+# print(f'Min of {s} is {min(s)}')
+# print(f'Length of {s} is {len(s)}')
+
+
+# String Methods
+# s=input("Enter your name:")
+# print(f'Lower of {s} is {s.lower()}')
+
+# s=input("Enter your name:")
+# print(f'Upper of {s} is {s.upper()}')
+
+# s=input("Enter your name:")
+# print(f'Title of {s} is {s.title()}')
+
+# s=input("Enter your name:")
+# print(f'Swap of {s} is {s.swapcase()}')
+
+# s=input("Enter your name:")
+# print(f'Capitalize of {s} is {s.capitalize()}')
+
+# s=input("Enter your name:")
+# ch=input("Enter any character:")
+# start=int(input("Enter start point:"))
+# stop=int(input("Enter stop point:"))
+# print(f'Index of elementm {ch} in {s} is {s.index(ch,start,stop)}')
+# print(input("Enter your name:").index(input("Enter any character:"),int(input("Enter start point:")),int(input("Enter stop point:"))))
+
+# s=input("Enter your name:")
+# ch=input("Enter any character:")
+print(input("Enter your name:").count(input("Enter any character:")))
+# print(f'Startswith of {s} is {s.startswith()}')
+
+# print(f'Endswith of {s} is {s.endswith()}')
+
+# print(f'Split of {s} is {s.split()}')
