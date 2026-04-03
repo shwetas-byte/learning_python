39b0d5c2d3d381e05f8817b427c7e1da68cec5bf
Shweta Singh <shwetas6913@gmail.com>
2026-04-03 13:28:59 +0530

    Learning Python

---
+++ my_list.py
@@ -0,0 +1,37 @@
+# l=eval(input("Enter any list:"))
+# print(l)
+# print(f'Type of {l} is {type(l)}')
+# print(f'Id of {l} is {id(l)}')
+# print(f'Length of {l} is {len(l)}')
+# print(f'Maximum of {l} is {max(l)}')
+# print(f'Minimum of {l} is {min(l)}')
+# print(f'Sum of {l} is {sum(l)}')
+
+
+
+# methods pf list
+
+# 1.copyy-creates new object with same elements
+# l=eval(input("Enter any list"))
+# l1=l.copy()
+# print(l,l1)
+# print(id(l),id(l1))
+
+# 2.clear-clear all elements from given list
+
+# 3.append--add single element at last position
+
+# 4.extend--add multiple elements at last position
+
+# 5.insert--add single element at targeted position
+
+# 6.pop--remove element from targeted indexing if not given by default len-1
+
+# 7.remove-remove particular element
+
+# 8.sort--arrange in ascending order
+
+# 9.reverse()--reverse out all the index position
+
+
+
