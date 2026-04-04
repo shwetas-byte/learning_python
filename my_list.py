76921fb88f71bfd0819e6f7acd891e25f25dbb86
Shweta Singh <shwetas6913@gmail.com>
2026-04-04 22:39:46 +0530

    Learning python

--- my_list.py
+++ my_list.py
@@ -16,10 +16,21 @@
 # l1=l.copy()
 # print(l,l1)
 # print(id(l),id(l1))
+# l=print(eval(input("Enter any list:")).copy())
+# print(l,id(l))
 
 # 2.clear-clear all elements from given list
+# l=eval(input("enter any list:"))
+# print(l)
+# l.clear()
+# print(l)
+
 
 # 3.append--add single element at last position
+# l=eval(input("Enter any list:"))
+# print(l)
+# l.append('java')
+# print(l)
 
 # 4.extend--add multiple elements at last position
 
