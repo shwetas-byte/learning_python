47a63c2f407e56fd1ee5f7c7d4e3e6759eb2d96f
Shweta Singh <shwetas6913@gmail.com>
2026-04-10 13:32:30 +0530

    Learning puthon

--- my_set.py
+++ my_set.py
@@ -33,11 +33,42 @@
 # s2={4,5,6,7,8}
 # print(s.symmetric_difference(s2))
 
+
+
 # 5 to 7 old -set-update
 # 5.intersection_update()
+# s={1,2,3,4,5}
+# s2={4,5,6,7,8}
+# s.intersection_update(s2) #koi value return nahi krta hai
+# print(s)
+
+
 # 6.difference_update()
+# s={1,2,3,4,5}
+# s2={4,5,6,7,8}
+# s.difference_update(s2)
+# print(s)
+
+
 # 7.symetric_difference_update()
+# s={1,2,3,4,5}
+# s2={4,5,6,7,8}
+# s.symmetric_difference_update(s2)
+# print(s)
 
 # 8 to 9 boolean
 # 8.issubset()
+# s={1,2,3,4,5}
+# s2={4,5}
+# print(s2.issubset(s))
+
 # 9.issuperset()
+# s={1,2,3,4,5,6,7,8}
+# s2={4,5,6,7,8}
+# print(s.issuperset(s2))
+# print(s)
+
+# 10.disjoint()
+s={1,2,3,4,5,6,7,8}
+s2={4,5,6,7}
+print(s.isdisjoint(s2))
