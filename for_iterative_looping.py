46f000d9551607a103f8aaa19e287a1ffec89eb7
Shweta Singh <shwetas6913@gmail.com>
2026-04-24 13:20:21 +0530

    learning python

--- for_iterative_looping.py
+++ for_iterative_looping.py
@@ -63,8 +63,96 @@
 # for i in s:
 #     print(i)
 
-n=int(input("enter any number:"))
-for i in range(1,n+1):
+# n=int(input("enter any number:"))
+# for i in range(1,n+1):
     # print('*'*i+''*(n-i))
     # print(' '*(n-i)+'*'*i)
     # print(' '*(n-i)+'* '*i)
+
+
+# n=int(input("Enter any number:"))
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         print(j,end=' ')
+#     print()
+
+
+# 1 
+# 1 2 
+# 1 2 3 
+# 1 2 3 4 
+# 1 2 3 4 5 
+# n=int(input("Enter any number:"))
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print(j,end=' ')
+#     print()
+
+# 1 
+# 2 3 
+# 4 5 6 
+# 7 8 9 10 
+# 11 12 13 14 15
+# n=int(input("Enter any integer:"))
+# x=1
+# for _ in range(1,n+1):
+#     for j in range(1,_+1):
+#         print(x,end=' ')
+#         x=x+1
+#     print()
+
+
+# 2 
+# 4 6 
+# 8 10 12 
+# 14 16 18 20 
+# 22 24 26 28 30 
+# n=int(input("Enter any integer:"))
+# x=2
+# for i in range(1,n+1):
+#     for _ in range(1,i+1):
+#         print(x,end=' ')
+#         x=x+2
+#     print()
+
+# 1 
+# 3 5 
+# 7 9 11 
+# 13 15 17 19 
+# 21 23 25 27 29 
+# n=int(input("Enter any integer:"))
+# x=1
+# for i in range(1,n+1):
+#     for _ in range(1,i+1):
+#         print(x,end=' ')
+#         x=x+2
+#     print()
+
+
+# A 
+# A B 
+# A B C 
+# A B C D 
+# A B C D E 
+# n=int(input("Enter any integer:"))
+# for i in range(1,n+1):
+#     x='A'
+#     for _ in range(1,i+1):
+#         print(x,end=' ')
+#         x=chr(ord(x)+1)
+#     print()
+
+
+# A 
+# B C 
+# D E F 
+# G H I J 
+# K L M N O 
+# n=int(input("Enter any integer:"))
+# x='A'
+# for i in range(1,n+1):
+#     for _ in range(1,i+1):
+#         print(x,end=' ')
+#         x=chr(ord(x)+1)       
+#     print()
+
