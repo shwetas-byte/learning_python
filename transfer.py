5cb9ea0f7648d0dc97b1ec248d3f0dbb11abd2c1
Shweta Singh <shwetas6913@gmail.com>
2026-04-24 13:36:03 +0530

    learning python

---
+++ transfer.py
@@ -0,0 +1,19 @@
+# comtinue--skip current iterarion
+# break--terminate current loop
+# pass--skip current block
+
+# n=10
+# i=0
+# while i<=10:
+    
+#     if i==5:
+#         i+=1  #ye agr nhi lgate toh infinite loop jayega
+#         continue
+#     print(i)
+#     i+=1
+
+# n=int(input("Enter no. of natural no. you want:"))
+# for i in range(1,n+1):
+#     if i==5:
+#         continue
+#     print(i)
