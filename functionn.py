918ac6bcc9e3f6d0c09e7b3d0cb0330e67bb6d1a
Shweta Singh <shwetas6913@gmail.com>
2026-05-06 13:11:58 +0530

    learning python

--- functionn.py
+++ functionn.py
@@ -10,25 +10,45 @@
 #     print('hello')
 # fun()
 
+# def greet():
+#     print("Welcome to our webpage")
+# greet()
+
+
 # without argument with return
 # def add():
 #     return 4+9
 # print('Addition is',add())
 
+# def greet():
+#     return 'welcome to our webpage'
+# print(greet())
+
+
 # with argument without return
 # def add(x,y):
 #     print('Addition is:',x+y)
-
 # x=int(input("enter first number:"))
 # y=int(input("enter first number:"))
 # add(x,y)
 
-# with return with argument
-def add(x,y):
-    add=x+y
-    return add
-x=int(input("enter first number:"))
-y=int(input("enter first number:"))
-print('Addition is:',add(x,y))
+# def greet(name):
+#     print(f'welcome {name}') 
+# name=input("Enter your name:")
+# greet(name)
 
+
+# with return with argument
+# def add(x,y):
+#     add=x+y
+#     return add
+# x=int(input("enter first number:"))
+# y=int(input("enter first number:"))
+# print('Addition is:',add(x,y))
+
+
+# def greet(name):
+#     return f'Welcome {name}'
+# name=input("Enter your name:")
+# print(greet(name))
 
