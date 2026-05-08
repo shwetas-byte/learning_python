e66fdb0c9c8b56e59e76d52c2bca0ef6a3a9b6b7
Shweta Singh <shwetas6913@gmail.com>
2026-05-08 13:31:21 +0530

    learning python

--- functionn.py
+++ functionn.py
@@ -55,10 +55,10 @@
 
 # Relation b/w parameter & arguments
 # 1.Positional argument
-def show(x,y,z):
-    print('x:',x)
-    print('y:',y)
-    print('z:',z)
+# def show(x,y,z):
+#     print('x:',x)
+#     print('y:',y)
+#     print('z:',z)
 # show(10,20,30)
 # show()   #show() missing 3 required positional arguments: 'x', 'y', and 'z'---|
 # show(10)   #show() missing 2 required positional arguments: 'y' and 'z'       |---- default positional argument
@@ -66,6 +66,43 @@ def show(x,y,z):
 # show(10,20,30,40)  #show() takes 3 positional arguments but 4 were given------|-----variable length argument
 
 
-
-
 # 2.Default positional argument
+# def add(x=0,y=0,z=0):
+#     print(x+y+z)
+# add()  #0
+# add(10) #10
+# add(10,20)  #30
+# add(10,20,30) #60
+# add(10,20,30,40)   #add() takes from 0 to 3 positional arguments but 4 were given
+
+
+# 3.variable length positional argument  (*args) --hold as an tuple datatype tuple
+# * it has packing function in parameter and unpacking in argument
+# def display(*args):
+#     print(args)
+#     print(type(args))
+# display()
+# display(10,20)
+# display(10,'python',[1,3,4,5])
+
+# def display(*n):
+#     sum=0
+#     for i in n:
+#         sum+=i
+#     print(sum)
+# display(10,20,30,40,50,60)
+
+# def display(*n):
+#     sum=0
+#     for i in n:
+#         sum+=i
+#     print('Addition is:',sum)
+# values=eval(input("Enter all values:"))
+# display(*values)  
+
+# natural no.
+# def natural_num(n):
+#     for i in range(1,n+1):
+#         print(i)
+# n=eval(input('enter how many natural no. you want:'))
+# natural_num(n)
